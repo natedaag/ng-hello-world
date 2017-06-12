6a6450e8e1c06c4f177475e8515aa579ef9679ae
natedaag <natedaag@natedaag-Satellite-L755>
2017-06-12 09:45:35 -0600

    Unnecessary decomposition (because Nick said so).

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+public class HelloWorld {
+	
+	private static void emitMessage(String message) {
+	 System.out.println(message);
+	}
+	 /**
+	 * Prints "Hello World!" to standard output.
+	 * @param args    Command-line parameters (ignored).
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		emitMessage("Hello World!");
+ 
+	
+	}
+}
+/**
+ * @author natedaag
+ * Simple class that displays the "Hello World!" message.
+ */
